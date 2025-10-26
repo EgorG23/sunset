42a3550fdc0c69ad1271f2c9901c14b9bb8ac543
EgorG23 <gusarow.egor2017@yandex.ru>
2025-10-27 00:40:08 +0300

    Update styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,23 @@ class AlbumRotator {
         this.albums = [
             {
                 id: 1,
-                title: "GUTS (spilled)",
-                artist: "Olivia Rodrigo",
                 html: `<iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:556px;border-radius:15px;" src="https://music.yandex.ru/iframe/album/30346230">Слушайте <a href="https://music.yandex.ru/album/30346230?utm_source=desktop&utm_medium=copy_link">GUTS (spilled)</a> — <a href="https://music.yandex.ru/artist/4654635">Olivia Rodrigo</a> на Яндекс Музыке</iframe>`
             },
             {
                 id: 2,
-                title: "HIT ME HARD AND SOFT",
-                artist: "Billie Eilish",
                 html: `<iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:556px;border-radius:15px;" src="https://music.yandex.ru/iframe/album/31266458">Слушайте <a href="https://music.yandex.ru/album/31266458?utm_source=desktop&utm_medium=copy_link">HIT ME HARD AND SOFT</a> — <a href="https://music.yandex.ru/artist/4353492">Billie Eilish</a> на Яндекс Музыке</iframe>`
             },
             {
                 id: 3,
-                title: "Man's Best Friend",
-                artist: "Sabrina Carpenter",
                 html: `<iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:556px;border-radius:15px;" src="https://music.yandex.ru/iframe/album/37999752">Слушайте <a href="https://music.yandex.ru/album/37999752">Man's Best Friend</a> — <a href="https://music.yandex.ru/artist/2001010">Sabrina Carpenter</a> на Яндекс Музыке</iframe>`
+            },
+            {
+                id: 4,
+                html: `<iframe frameborder="0" allow="clipboard-write" style="border:none;width:614px;height:556px;" width="614" height="556" src="https://music.yandex.ru/iframe/album/34065401">Слушайте <a href="https://music.yandex.ru/album/34065401?utm_source=desktop&utm_medium=copy_link">Die With A Smile</a> — <a href="https://music.yandex.ru/artist/1438">Lady Gaga</a> на Яндекс Музыке</iframe>`
+            },
+            {
+                id: 5,
+                html: `<iframe frameborder="0" allow="clipboard-write" style="border:none;width:614px;height:556px;" width="614" height="556" src="https://music.yandex.ru/iframe/album/37196930">Слушайте <a href="https://music.yandex.ru/album/37196930?utm_source=desktop&utm_medium=copy_link">Virgin</a> — <a href="https://music.yandex.ru/artist/1654436">Lorde</a> на Яндекс Музыке</iframe>`
             }
         ];
         this.currentAlbumIndex = this.getMinuteOfHour() % this.albums.length;
@@ -45,23 +47,10 @@ class AlbumRotator {
         const currentAlbum = this.albums[this.currentAlbumIndex];
         
         if (container) {
-            container.innerHTML = `
-                <div class="album-info">
-                    <h3>${currentAlbum.title}</h3>
-                    <p class="album-artist">${currentAlbum.artist}</p>
-                </div>
-                ${currentAlbum.html}
-            `;
-            this.updateSectionTitle(currentAlbum);
+            container.innerHTML = currentAlbum.html;
         }
     }
 
-    updateSectionTitle(album) {
-        const widgetSection = document.querySelector('.spotify-widget h2');
-        if (widgetSection) {
-            widgetSection.innerHTML = `🎵 Альбом дня: <span class="album-title">${album.title}</span>`;
-        }
-    }
 
     init() {
         this.updateWidget();
